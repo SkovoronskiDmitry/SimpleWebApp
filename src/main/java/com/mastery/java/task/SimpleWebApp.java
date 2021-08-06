54cd50596c00040fdeeb019c5aea4b3337f3aa40
dmitry <dmitryskovoronski@gmail.com>
2021-08-06 18:09:08 +0300

    add POJO class

--- src/main/java/com/mastery/java/task/SimpleWebApp.java
+++ src/main/java/com/mastery/java/task/SimpleWebApp.java
@@ -2,8 +2,10 @@ package com.mastery.java.task;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 
-@SpringBootApplication
+@SpringBootApplication(exclude =
+        DataSourceAutoConfiguration.class)
 public class SimpleWebApp {
 
     public static void main(final String[] args) {
--- src/main/java/com/mastery/java/task/dto/Employee.java
+++ src/main/java/com/mastery/java/task/dto/Employee.java
@@ -1,8 +1,153 @@
 package com.mastery.java.task.dto;
 
+import io.swagger.annotations.ApiModel;
+import io.swagger.annotations.ApiModelProperty;
+import lombok.AllArgsConstructor;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.validation.constraints.*;
+import java.time.LocalDate;
+import java.util.Objects;
+
+
+@ApiModel(value = "Employee Model", subTypes = {Employee.class}, description = "POJO class for Swagger documentation")
+@Entity
+@NoArgsConstructor
+@AllArgsConstructor
 public class Employee {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @NotNull(message = "Id can't be null")
+    @Positive(message = "Only positive number")
     private Long employeeId;
+    @NotEmpty(message = "The FirstName value can't be empty")
     private String firstName;
-    private Gender gender;
+    @NotEmpty(message = "The LastName value can't be empty")
+    private String lastName;
+    @NotNull(message = "The Gender value can't be empty")
+    private String gender;
+    @Positive(message = "Only positive number")
+    @NotNull(message = "The Department ID value can't be null")
+    private Long departmentId;
+    @NotEmpty(message = "The Job Title value can't be empty")
+    private String jobTitle;
+    @Past(message = "Only past time")
+    private LocalDate dateOfBirth;
+
+    public Employee(
+            final String firstName,
+            final String lastName,
+            final String gender,
+            final Long departmentId,
+            final String jobTitle,
+            final LocalDate dateOfBirth
+    ) {
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.gender = gender;
+        this.departmentId = departmentId;
+        this.jobTitle = jobTitle;
+        this.dateOfBirth = dateOfBirth;
+    }
+
+    @ApiModelProperty(value = "Unique number id", example = "1", required = true, position = 1)
+    public Long getEmployeeId() {
+        return employeeId;
+    }
+
+    public void setEmployeeId(final Long employeeId) {
+        this.employeeId = employeeId;
+    }
+
+    @ApiModelProperty(value = "Employee firstname", example = "John", required = true, position = 2)
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(final String firstName) {
+        this.firstName = firstName;
+    }
+
+    @ApiModelProperty(value = "Employee lastname", example = "Smith", required = true, position = 3)
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(final String lastName) {
+        this.lastName = lastName;
+    }
+
+    @ApiModelProperty(value = "Gender identity", allowableValues = "MALE, FEMALE", required = true, position = 5)
+    public String getGender() {
+        return gender.toString();
+    }
+
+    public void setGender(final String gender) {
+        this.gender = gender;
+    }
+
+    @ApiModelProperty(value = "Employee's department number", example = "9", required = true, position = 6)
+    public Long getDepartmentId() {
+        return departmentId;
+    }
+
+    public void setDepartmentId(final Long departmentId) {
+        this.departmentId = departmentId;
+    }
+
+    @ApiModelProperty(value = "Employee's job title", example = "boss", required = true, position = 7)
+    public String getJobTitle() {
+        return jobTitle;
+    }
+
+    public void setJobTitle(final String jobTitle) {
+        this.jobTitle = jobTitle;
+    }
+
+    @ApiModelProperty(value = "Employee's birthday", example = "2007-12-25", required = true, position = 4)
+    public LocalDate getDateOfBirth() {
+        return dateOfBirth;
+    }
+
+    public void setDateOfBirth(final LocalDate dateOfBirth) {
+        this.dateOfBirth = dateOfBirth;
+    }
+
+    @Override
+    public boolean equals(final Object o) {
+        if (this == o) {
+            return true;
+        } else if (o == null || getClass() != o.getClass()) {
+            return false;
+        }
+
+        final Employee employee = (Employee) o;
+        return employeeId.equals(employee.employeeId)
+                && firstName.equals(employee.firstName)
+                && lastName.equals(employee.lastName);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(employeeId, departmentId, firstName, lastName);
+    }
+
+    @Override
+    public String toString() {
+        return "Employee{" +
+                "employeeId=" + employeeId +
+                ", firstName='" + firstName + '\'' +
+                ", lastName='" + lastName + '\'' +
+                ", gender=" + gender +
+                ", departmentId=" + departmentId +
+                ", jobTitle='" + jobTitle + '\'' +
+                ", dateOfBirth=" + dateOfBirth +
+                '}';
+    }
 
 }
