16d0da121a02a6e578a0b9f7c307bb62765cb41b
dmitry <dmitryskovoronski@gmail.com>
2021-08-08 21:34:18 +0300

    refactoring DateTimeService

--- src/main/java/com/mastery/java/task/service/datetime/DateTimeService.java
+++ src/main/java/com/mastery/java/task/service/datetime/DateTimeService.java
@@ -1,7 +1,10 @@
 package com.mastery.java.task.service.datetime;
 
+import org.springframework.stereotype.Service;
+
 import java.util.Date;
 
+@Service
 public class DateTimeService {
 
     public Date getCurrentDate() {
